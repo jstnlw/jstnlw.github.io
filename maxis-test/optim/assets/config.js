6741b798ade8ca2755b101c5e5bd0f4ab9e2bf74
Justin Low <justin.low@prodigious.com>
2023-01-16 18:13:05 +0800

    no message

---
+++ maxis-test/optim/assets/config.js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C6FG458B3BVPD5SJNRA0").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true,"auto_email":false,"auto_phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C6FG458B3BVPD5SJNRA0").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C6FG458B3BVPD5SJNRA0").setPixelInfo({status: 0, name: "Hotlink Developer Pixel", advertiserID: "6880722074701135874", setupMode: 1, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("maxis.com.my");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig(null);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(true);
